a9d65ea9f6c2b80bafb12173923b9c2a53cdd38c
史宇韬 <yutao.shi@baishancloud.com>
2018-05-04 15:23:31 +0800

    debug

--- command/sync.js
+++ command/sync.js
@@ -7,52 +7,52 @@
 module.exports = function (supplement, syncCallback, codeCallback) {
   _require('/lib/utils').then(({ bsy: { name: project, builder }, readFiles, uid, connect }) => {
     watch(connect(), supplement, syncCallback, codeCallback)
-  })
 
-  function watch(socket, supplement, syncCallback, codeCallback) {
-    const path = require('path')
+    function watch(socket, supplement, syncCallback, codeCallback) {
+      const path = require('path')
 
-    Promise.all([
-      _require('/lib/signal'),
-      _require('/lib/NodeWatchFileSystem')
-    ]).then(([signal, NodeWatchFileSystem]) => {
-      const wfs = new NodeWatchFileSystem(socket)
+      Promise.all([
+        _require('/lib/signal'),
+        _require('/lib/NodeWatchFileSystem')
+      ]).then(([signal, NodeWatchFileSystem]) => {
+        const wfs = new NodeWatchFileSystem(socket)
 
-      // 建连后与服务端同步项目
-      socket.connected.then(init)
-      socket.on('data', data => signal.decode(data).forEach(info => handle(socket, info)))
+        // 建连后与服务端同步项目
+        socket.connected.then(init)
+        socket.on('data', data => signal.decode(data).forEach(info => handle(socket, info)))
 
-      codeCallback && codeCallback(wfs)
+        codeCallback && codeCallback(wfs)
 
-      /**
-       * 初始化连接
-       */
-      function init() {
-        console.log(signal.encode(
-          signal.CLIENT_INIT,
-          Buffer.from(JSON.stringify({ uid, project, builder }))
-        ))
+        /**
+         * 初始化连接
+         */
+        function init() {
+          console.log(signal.encode(
+            signal.CLIENT_INIT,
+            Buffer.from(JSON.stringify({ uid, project, builder }))
+          ))
 
-        socket.write(signal.encode(
-          signal.CLIENT_INIT,
-          Buffer.from(JSON.stringify({ uid, project, builder }))
-        ))
-      }
-
-      /**
-       * 接收信息处理
-       * @param {Socket} socket
-       * @param {Object} { type: 信号位, content: 传输内容, note: 附加信息 }
-       */
-      function handle(socket, {type, content, note}) {
-        switch (type) {
-          case signal.SERVER_INIT_DONE: return wfs.sync(readFiles(process.cwd(), /node_modules|dist|\.git/))
-          case signal.SERVER_FILE_UPDATE: return wfs.fileUpdate(path.resolve(note.toString()), true)
-          case signal.SERVER_CHECK_OFF: return syncCallback && syncCallback(socket)
-          case signal.SERVER_CONSOLE: return console.log(content.toString())
-          default: return supplement && supplement(socket, {type, content, note})
+          socket.write(signal.encode(
+            signal.CLIENT_INIT,
+            Buffer.from(JSON.stringify({ uid, project, builder }))
+          ))
         }
-      }
-    })
-  }
+
+        /**
+         * 接收信息处理
+         * @param {Socket} socket
+         * @param {Object} { type: 信号位, content: 传输内容, note: 附加信息 }
+         */
+        function handle(socket, {type, content, note}) {
+          switch (type) {
+            case signal.SERVER_INIT_DONE: return wfs.sync(readFiles(process.cwd(), /node_modules|dist|\.git/))
+            case signal.SERVER_FILE_UPDATE: return wfs.fileUpdate(path.resolve(note.toString()), true)
+            case signal.SERVER_CHECK_OFF: return syncCallback && syncCallback(socket)
+            case signal.SERVER_CONSOLE: return console.log(content.toString())
+            default: return supplement && supplement(socket, {type, content, note})
+          }
+        }
+      })
+    }
+  })
 }
