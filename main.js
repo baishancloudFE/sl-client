39fc83a63231350d1b4c11f6199a65776a90e728
史宇韬 <yutao.shi@baishancloud.com>
2018-05-07 19:46:37 +0800

    debug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
   if (config.dev) return _require('/command/dev').then(dev => dev(config))
-  if (config.build) return _require('/command/build')(config)
+  if (config.build) return _require('/command/build').then(build => build(config))
   if (config.init) return _require('/command/init')()
   if (config['set-source']) return _require('/command/set-source').then(setSource => setSource(config['set-source']))
   if (config['set-server']) return _require('/command/set-server').then(setServer => setServer(config['set-server']))
